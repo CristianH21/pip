e56070c11fab822deca352501f2dcca3ab1ad4cc
CristianH21 <crishhtona21@gmail.com>
2020-05-24 13:59:30 -0500

    port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 // Init middleware
 app.use(express.json({ extended: false }));
