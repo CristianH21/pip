542e3b3536520eddc8c5f09d92761edcbe22e6b3
CristianH21 <crishhtona21@gmail.com>
2020-05-24 09:58:48 -0500

    cors config update 3

--- index.js
+++ index.js
@@ -6,20 +6,34 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Init middleware
-const corsOptions = {
-    origin: 'http://institutopatriaprimaria.com',
-    methods: ['GET', 'PUT', 'POST'],
-    allowedHeaders: ['Content-Type', 'x-auth-token']
-}
-app.use(cors(corsOptions));
 app.use(express.json({ extended: false }));
 
 // Init routes
-app.use('/api/auth', require('./routes/auth.route'));
-app.use('/api/profile', require('./routes/profile.route'));
-app.use('/api/users', require('./routes/users.route'));
-app.use('/api/subjects', require('./routes/subjects.route'));
-app.use('/api/groups', require('./routes/groups.route'));
+app.use(
+    '/api/auth', 
+    cors({ origin: 'http://institutopatriaprimaria.com' }),
+    require('./routes/auth.route')
+);
+app.use(
+    '/api/profile', 
+    cors({ origin: 'http://institutopatriaprimaria.com' }),
+    require('./routes/profile.route')
+);
+app.use(
+    '/api/users', 
+    cors({ origin: 'http://institutopatriaprimaria.com' }),
+    require('./routes/users.route')
+);
+app.use(
+    '/api/subjects',
+    cors({ origin: 'http://institutopatriaprimaria.com' }),
+    require('./routes/subjects.route')
+);
+app.use(
+    '/api/groups',
+    cors({ origin: 'http://institutopatriaprimaria.com' }),
+    require('./routes/groups.route')
+);
 
 app.listen(PORT,  () => console.log(`Server running on port 5000`));
 
