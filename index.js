15c4451c477bef504041ffd24b371cc48b1844c3
CristianH21 <crishhtona21@gmail.com>
2020-05-24 21:25:57 -0500

    config 2

--- index.js
+++ index.js
@@ -9,16 +9,24 @@ app.use(express.json({ extended: false }));
 
 // CORS
 //CORS
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.setHeader(
+//         "Access-Control-Allow-Methods", 
+//         "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//     );
+//     next();
+// });
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods", 
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next();
 });
 
