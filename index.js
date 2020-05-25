fead9284ed1a50fbc377040253586c7ce4672b66
CristianH21 <crishhtona21@gmail.com>
2020-05-24 21:27:27 -0500

    config token

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
     next();
 });
 
